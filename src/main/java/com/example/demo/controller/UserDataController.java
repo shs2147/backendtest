5edd94cc6fe138d9a261aa4f3e3a034d8b57d07c
HarshNiranjan <harshniranjan@monocept.com>
2024-05-08 15:03:56 +0530

    cors2

--- src/main/java/com/example/demo/controller/UserDataController.java
+++ src/main/java/com/example/demo/controller/UserDataController.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/userData")
-
+@CrossOrigin(origins = "*")
 public class UserDataController {
     @Autowired
     private UserDataService userDataService;
